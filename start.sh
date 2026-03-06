6bc050cb600a32693b7e4dbad7816d067d6ecbb4
EAGLEEARTH <ufukcicek199@gmail.com>
2026-03-06 23:01:58 +0300

    push

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -e
 export PORT=${PORT:-8000}
+# Migrate önce; bittikten sonra gunicorn başlar (healthcheck bu sıraya göre çalışır)
+python manage.py migrate --noinput
 exec gunicorn marifetli_project.wsgi:application --bind "0.0.0.0:$PORT" --workers 3
